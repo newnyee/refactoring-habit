5f71da321ca6c37545dc22bd418e59195ebab359
newnyee <mebbun0615@gmail.com>
2024-05-06 12:58:05 +0900

    Initial commit

---
+++ src/main/java/com/refactoringhabit/RefactoringHabitApplication.java
@@ -0,0 +1,13 @@
+package com.refactoringhabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringHabitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefactoringHabitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/refactoringhabit/RefactoringHabitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refactoringhabit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringHabitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
