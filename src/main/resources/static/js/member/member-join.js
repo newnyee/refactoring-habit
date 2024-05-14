793d4c2aaadae55f07a39c2d18dfb11222ac12a7
newnyee <mebbun0615@gmail.com>
2024-05-14 12:05:03 +0900

    feat: member-join.js validation 추가

--- src/main/resources/static/js/member/member-join.js
+++ src/main/resources/static/js/member/member-join.js
@@ -1,95 +1,225 @@
-function setProfile(event) {
-    var reader = new FileReader();
-
-    reader.onload = function(event) {
-      $('.Home_proimg').attr('src',event.target.result);
-    };
-
-    reader.readAsDataURL(event.target.files[0]);
+// 필수 입력 확인
+const notNullCheck = (value, errorMessage) => {
+  if (value.length === 0 || value === '') {
+    errorMessage.css('display', 'block')
+    errorMessage.text('필수 입력란 입니다')
+    return false
   }
-
-function checkLength(){
-
-
-    if($('#user_phone').val().length!=3){
-        $('#user_phone').val( $('#user_phone').val().slice(0,3));
-    }
-
-    if($('#user_phone2').val().length!=4){
-        $('#user_phone2').val($('#user_phone2').val().slice(0,4));
-    }
-
-    if($('#user_phone3').val().length!=4){
-        $('#user_phone3').val( $('#user_phone3').val().slice(0,4));
-    }
-
 }
 
+// 이메일 검증
+const emailValidation = () => {
+  let email = $('#email')
+  let emailErrorMessage
+      = email.closest('.Home_form_div')
+        .find('.error-message')
 
-  function join_check() {
-      //변수에 담아주기
-      let user_id = document.getElementById("user_id");
-      let user_pw = document.getElementById("user_pw");
-      let user_name = document.getElementById("user_name");
-      let user_gender = document.getElementById("user_gender");
-      let user_phone = document.getElementById("user_phone");
-      let user_phone2 = document.getElementById("user_phone2");
-      let user_phone3 = document.getElementById("user_phone3");
-      let user_email = document.getElementById("user_email");
-      let user_email2 = document.getElementById("user_email2");
-      let user_birth = document.getElementById("user_birth");
+  let emailCheck = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/
 
-      if (user_id.value == ""||user_id.value.length < 5) { //해당 입력값이 없을 경우 같은말: if(!uid.value)
-          alert("아이디 중복확인해주세요.");
-          user_id.focus(); //focus(): 커서가 깜빡이는 현상, blur(): 커서가 사라지는 현상
-          return false; //return: 반환하다 return false:  아무것도 반환하지 말아라 아래 코드부터 아무것도 진행하지 말것
-      };
+  notNullCheck(email.val(), emailErrorMessage)
 
-      if (user_pw.value == ""||user_pw.value.length<8) {
-          alert("비밀번호 8자 이상 입력해주세요");
-          user_pw.focus();
-          return false;
-      };
+  if (emailCheck.test(email.val()) === false) {
+    emailErrorMessage.css('display', 'block')
+    emailErrorMessage.text('이메일 형식에 맞게 입력해주세요')
+    return false
+  }
 
+  emailErrorMessage.css('display', 'none')
+}
 
-      // 아이디 특수문자 검사
- /*     var re = /^[a-zA-Z0-9_!@#$%^&*()-]+$/;
-      if (!re.test(id)) {
-          alert("아이디는 영문자, 숫자, 특수문자(_!@#$%^&*())만 사용 가능합니다.");
-          return false;
-      }*/
+// 비밀번호 확인 검증
+const passwordMatch = () => {
+  let password = $('#password')
+  let passwordMatch = $('#password_match')
+  let passwordMatchErrorMessage
+      = passwordMatch.closest('.Home_form_div').find('.error-message')
 
-      // 닉네임 길이 검사
-      if (user_name.value.length < 3) {
-          alert("닉네임은 3자 이상이어야 합니다.");
-          user_name.focus();
-          return false;
-      }
+  if (password.val() !== passwordMatch.val()) {
+    passwordMatchErrorMessage.css('display', 'block')
+    passwordMatchErrorMessage.text('비밀번호가 다릅니다')
+    return false
+  }
 
-      //성별검사
-      if($("input[name='user_gender']:radio:checked").length<1){
-          alert("성별을 선택해주세요");
-          $("#user_gender").focus();
-          return false;
-      }
+  passwordMatchErrorMessage.css('display', 'none')
+}
 
-      //이메일
-      if(user_email.value==""||$("select[name='user_email2']").val() == "0"){
-          alert("이메일을 입력해주세요.");
-          user_email.focus();
-          return false;
-      }
-      //후대폰번호
-      if(user_phone.value==""||user_phone2.value==""||user_phone3.value==""){
-          alert("휴대폰 번호를 입력해주세요");
-          document.getElementById("user_phone").focus();
-          return false;
-      }
+// 비밀번호 검증
+const passwordValidation = () => {
+  let password = $('#password')
+  let passwordErrorMessage
+      = password.closest('.Home_form_div')
+        .find('.error-message')
 
-      if(user_birth.value==""){
-          alert("생년월일을 선택해주세요");
-          document.getElementById("user_birth").focus();
-          return  false;
+  notNullCheck(password.val(), passwordErrorMessage)
 
-    }   
+  let match = $('#password_match')
+  if (match.val().length > 0) {
+    passwordMatch()
+  }
+
+  if (password.val().length < 8 || password.val().length > 20) {
+    passwordErrorMessage.css('display', 'block')
+    passwordErrorMessage.text('8~20자 이내로 입력해주세요')
+    return false
+  }
+
+  if (password.val().search(/\s/) !== -1) {
+    passwordErrorMessage.css('display', 'block')
+    passwordErrorMessage.text('비밀번호는 공백 없이 입력해주세요')
+    return false
+  }
+
+  if (
+      password.val().search(/[0-9]/g) < 0 || // 숫자
+      password.val().search(/[a-z]/ig) < 0 || // 영어
+      password.val().search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi) < 0 // 특수문자
+  ) {
+    passwordErrorMessage.css('display', 'block')
+    passwordErrorMessage.text('영문, 숫자, 특수문자를 혼합하여 입력해주세요')
+    return false
+  }
+
+  passwordErrorMessage.css('display', 'none')
+}
+
+// 닉네임 검증
+const nickNameValidation = () => {
+  let nickName = $('#nick_name')
+  let nickNameErrorMessage = nickName.closest('.Home_form_div').find('.error-message')
+
+  notNullCheck(nickName.val(), nickNameErrorMessage)
+
+  if (nickName.val().length < 2) {
+    nickNameErrorMessage.css('display', 'block')
+    nickNameErrorMessage.text('닉네임을 2자 이상 입력해주세요')
+    return false
+  }
+
+  nickNameErrorMessage.css('display', 'none')
+}
+
+// 성별 검증
+const genderValidation = () => {
+  let genderErrorMessage = $('#gender_male').closest('.Home_form_div').find('.error-message')
+
+  if ($("input[name='gender']:radio:checked").length < 1) {
+    genderErrorMessage.css('display', 'block')
+    genderErrorMessage.text('성별을 선택해주세요')
+    return false
+  }
+
+  genderErrorMessage.css('display', 'none')
+}
+
+// 휴대폰 번호 검증
+const phoneNumberLengthLimit = () => {
+  let phoneNumbers = $("input[name='phone']")
+  let firstNumber = phoneNumbers.eq(0)
+  let middleNumber = phoneNumbers.eq(1)
+  let lastNumber = phoneNumbers.eq(2)
+  let phoneErrorMessage = firstNumber.closest('.Home_form_div').find('.error-message')
+
+  if(firstNumber.val().length !== 3){
+    firstNumber.val(firstNumber.val().slice(0,3));
+  }
+
+  if(middleNumber.val().length !== 4){
+    middleNumber.val(middleNumber.val().slice(0,4));
+  }
+
+  if(lastNumber.val().length !== 4){
+    lastNumber.val(lastNumber.val().slice(0,4));
+  }
+
+  for (let i = 0; i<phoneNumbers.length; i++) {
+    if (phoneNumbers.eq(i).val().length === 0) {
+      phoneErrorMessage.css('display', 'block')
+      phoneErrorMessage.text('필수 입력란 입니다')
+      return false
+    }
+  }
+
+  phoneErrorMessage.css('display', 'none')
+}
+
+// 생년월일 검증
+const birthValidation = () => {
+  let birth = $('#birth')
+  let birthErrorMessage = birth.closest('.Home_form_div').find('.error-message')
+
+  notNullCheck(birth.val(), birthErrorMessage)
+  //
+  // if (birth.val() === '') {
+  //   birthErrorMessage.css('display', 'block')
+  //   birthErrorMessage.text('필수 입력란 입니다')
+  // }
+
+  let year = birth.val().split('-')[0];
+  let month = birth.val().split('-')[1]
+  let date = birth.val().split('-')[2]
+
+  let inputValue = new Date(year, month, date)
+  let now = new Date()
+  if (year < now.getFullYear() - 100 || inputValue > now) {
+    birthErrorMessage.css('display', 'block')
+    birthErrorMessage.text('생년월일을 확인해주세요')
+    return false
+  }
+
+  birthErrorMessage.css('display', 'none');
+}
+
+// 이미지 파일 검증
+const previewProfile = (imgs) => {
+  let reader = new FileReader();
+  let profileImageErrorMessage = $('.Home_form_div_p').find('.error-message')
+
+  if (imgs !== undefined) {
+    let imgSize = imgs.files[0].size // 이미지 사이즈
+    let maxSize = 2 * 1024 *1024 // 2MB
+    let fileExtension = imgs.files[0].name.split('\.').slice(-1)[0] // 파일 확장자
+
+    if (imgSize > maxSize || (fileExtension !== 'png' && fileExtension !== 'jpg')) {
+      profileImageErrorMessage.css('display', 'block')
+      profileImageErrorMessage.text('2MB 이하의 png, jpg 파일만 가능합니다')
+      imgs = undefined
+      return false
+    }
+
+    reader.onload = (img) => {
+      $('#preview').attr('src', img.target.result)
+    }
+    reader.readAsDataURL(imgs.files[0]);
+  } else {
+    $('#preview').attr('src', '/img/noimg.png')
+  }
+
+  profileImageErrorMessage.css('display', 'none')
+}
+
+const joinValidation = () => {
+
+  // 이메일
+  emailValidation()
+
+  // 비밀번호
+  passwordValidation()
+
+  // 비밀번호 확인
+  passwordMatch()
+
+  // 닉네임
+  nickNameValidation()
+
+  //성별
+  genderValidation()
+
+  // 휴대폰 번호
+  phoneNumberLengthLimit()
+
+  // 생년월일
+  birthValidation()
+
+  // 이미지 파일
+  previewProfile()
 }
