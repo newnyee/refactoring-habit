8d7557cff2a8e9de2f83fb80db51befa24a5b992
newnyee <mebbun0615@gmail.com>
2024-05-16 17:16:28 +0900

    feat: Mapstruct를 이용한 MemberEntityMapper 생성
    - MemberJoinRequestDto 를 Entity로 변환할 toEntity 메서드 생성
    - MemberJoinRequestDto에 없는 mid(UUID) 매핑을 위한 default 매서드 (generateUuid()) 생성

---
+++ src/main/generated/com/refactoringhabit/member/domain/mapper/MemberEntityMapperImpl.java
@@ -0,0 +1,40 @@
+package com.refactoringhabit.member.domain.mapper;
+
+import com.refactoringhabit.member.domain.entity.Member;
+import com.refactoringhabit.member.domain.enums.Gender;
+import com.refactoringhabit.member.dto.MemberJoinRequestDto;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-05-16T16:48:01+0900",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.6 (Oracle Corporation)"
+)
+@Component
+public class MemberEntityMapperImpl implements MemberEntityMapper {
+
+    @Override
+    public Member toEntity(MemberJoinRequestDto memberJoinRequestDto, String mid) {
+        if ( memberJoinRequestDto == null && mid == null ) {
+            return null;
+        }
+
+        Member.MemberBuilder member = Member.builder();
+
+        if ( memberJoinRequestDto != null ) {
+            member.email( memberJoinRequestDto.getEmail() );
+            member.password( memberJoinRequestDto.getPassword() );
+            member.nickName( memberJoinRequestDto.getNickName() );
+            member.phone( memberJoinRequestDto.getPhone() );
+            member.birth( memberJoinRequestDto.getBirth() );
+            member.profileImage( memberJoinRequestDto.getProfileImage() );
+            if ( memberJoinRequestDto.getGender() != null ) {
+                member.gender( Enum.valueOf( Gender.class, memberJoinRequestDto.getGender() ) );
+            }
+        }
+        member.mid( generateUuid( mid ) );
+
+        return member.build();
+    }
+}
