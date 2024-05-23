86b8e5fe6e1b462a6e860c5640ca53cf347c0acc
newnyee <mebbun0615@gmail.com>
2024-05-24 00:23:16 +0900

    feat: AuthController 생성
    - 아이디, 비밀번호 찾기 관련 뷰 컨트롤러 생성

---
+++ src/main/java/com/refactoringhabit/auth/controller/AuthController.java
@@ -0,0 +1,23 @@
+package com.refactoringhabit.auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/find-member")
+    public String findMember() {
+        return "/pages/member/find-member";
+    }
+
+    @GetMapping("/find-member/id")
+    public String findId() {
+        return "/pages/member/find-id";
+    }
+
+    @GetMapping("/find-member/password")
+    public String findPassword() {
+        return "/pages/member/find-password";
+    }
+}
