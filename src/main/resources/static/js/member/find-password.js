0fc7f9e6b330b458fe3a57a804bd8e6fc2c73aad
newnyee <mebbun0615@gmail.com>
2024-05-24 04:40:07 +0900

    feat: find-password.js 로직 추가
    - 아이디/비밀번호 찾기 페이지에서 sessionStorage에 찾아낸 email 저장 → 저장된 email을 화면에 출력하여 사용자에게 이메일을 알려줌
    - sessionStorage에 저장된 email이 없는경우 아이디/비밀번호 찾기 페이지로 이동시킴
    - 임시 비밀번호 API 호출 함수 추가

--- src/main/resources/static/js/member/find-password.js
+++ src/main/resources/static/js/member/find-password.js
@@ -1,42 +1,50 @@
-$(document).ready(()=>{
-  window.onload=function(){
-       //common.js
-       common();
+$(document).ready(()=> {
+  let responseData = sessionStorage.getItem('responseData')
+  if (responseData === null) {
+    alert('핸드폰번호, 생년월일을 먼저 인증해주세요.')
+    window.location.href = '/find-member'
+
+  } else {
+    let email = JSON.parse(responseData).data
+    let showEmail = ''
+
+    for (let i = 0; i < email.length; i++) {
+      if (i < 4 || email[i] === '@' || (i >= 4 && email[i] === '\.')) {
+        showEmail += email[i]
+      } else {
+        showEmail += '*'
+      }
+    }
+
+    $('#found_email').text(showEmail)
   }
 
   $('#sendEmail').on('click', ()=>{
 
-       if (confirm("임시 비밀번호를 발급받으시겠습니까?")) {
-            let checkMethod = $('input[type=radio][name="method"]:checked')
-            let methodOfFine = checkMethod.attr("id")
-            let methodValue = checkMethod.val()
+    if (confirm("임시 비밀번호를 발급받으시겠습니까?")) {
+      let responseData = sessionStorage.getItem('responseData')
 
-            let requestData = {
-                 'methodOfFine': methodOfFine,
-                 'methodValue': methodValue
-            }
+      $.ajax({
+        url: '/api/v2/auth/reset-password',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.parse(responseData).data,
+        success: () => {
+          sessionStorage.removeItem('responseData')
+          $('#sendEmailContainer').attr('hidden', true)
+          $('#result').attr('hidden', false)
+        },
+        beforeSend: () => {
+          $('#div_load_img').css('display', 'flex')
+        },
+        complete: () => {
+          $('#div_load_img').css('display', 'none')
+        }
+      })
 
-            $.ajax({
-                 url: '/findpw/result',
-                 type: 'post',
-                 dataType: 'json',
-                 data: requestData,
-                 success: (result) => {
-                      if (result == 1) {
-                           $('#methodSelect').attr('hidden', true);
-                           $('#result').removeAttr('hidden')
-                      }
-                 },
-                 beforeSend: () => {
-                      $('#div_load_img').css('display', 'flex')
-                 },
-                 complete: () => {
-                      $('#div_load_img').css('display', 'none')
-                 }
-            })
-       } else {
-            return false
-       }
-
+    } else {
+      sessionStorage.removeItem('responseData')
+      return false
+    }
   })
 })
