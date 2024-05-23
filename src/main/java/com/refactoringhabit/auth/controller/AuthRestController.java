7d6afe9978a871bf47e032637770573e4a14fb04
newnyee <mebbun0615@gmail.com>
2024-05-24 04:43:55 +0900

    feat: 임시 비밀번호 발급 API 생성
    - 임시 비밀번호 발급하여 DB에 저장되어있는 기존의 비밀번호 변경
    - 새로 생성된 임시 비밀번호를 해당 이메일로 전송

--- src/main/java/com/refactoringhabit/auth/controller/AuthRestController.java
+++ src/main/java/com/refactoringhabit/auth/controller/AuthRestController.java
@@ -4,11 +4,13 @@ import com.refactoringhabit.auth.domain.service.AuthService;
 import com.refactoringhabit.auth.dto.FindEmailRequestDto;
 import com.refactoringhabit.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v2/auth")
@@ -19,5 +21,12 @@ public class AuthRestController {
     @PostMapping("/find-email")
     public ApiResponse<String> findEmail(@RequestBody FindEmailRequestDto findEmailRequestDto) {
         return ApiResponse.ok(authService.findEmail(findEmailRequestDto));
+    }
+
+    @PostMapping("/reset-password")
+    public ApiResponse<String> resetPassword(@RequestBody String email) {
+        log.debug("request data email : {}", email);
+        authService.resetPassword(email);
+        return ApiResponse.noContent();
     }
 }
--- src/main/java/com/refactoringhabit/member/domain/repository/MemberRepository.java
+++ src/main/java/com/refactoringhabit/member/domain/repository/MemberRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
     Boolean existsByEmail(String email);
+
+    Member findByEmail(String email);
 }
