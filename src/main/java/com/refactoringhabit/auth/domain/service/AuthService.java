f19adca16223054ddb9710ba9c4ad6dd7b108242
newnyee <mebbun0615@gmail.com>
2024-05-31 03:09:00 +0900

    feat: 서비스 메서드 추가
    - authenticationAndCreateToken() : 인증 후 토큰 발급
    - reissueToken() : 토큰 재발급

--- src/main/java/com/refactoringhabit/auth/domain/service/AuthService.java
+++ src/main/java/com/refactoringhabit/auth/domain/service/AuthService.java
@@ -1,13 +1,23 @@
 package com.refactoringhabit.auth.domain.service;
 
 import com.refactoringhabit.auth.domain.exception.EmailingException;
+import com.refactoringhabit.auth.domain.exception.InvalidTokenException;
+import com.refactoringhabit.auth.domain.repository.RedisRefreshTokenRepository;
 import com.refactoringhabit.auth.dto.FindEmailRequestDto;
+import com.refactoringhabit.auth.dto.SignInRequestDto;
+import com.refactoringhabit.auth.dto.SignInResponseDto;
 import com.refactoringhabit.common.annotation.Timer;
+import com.refactoringhabit.common.response.TokenResponse;
+import com.refactoringhabit.common.utils.cookies.CookieUtil;
 import com.refactoringhabit.common.utils.EmailNewPasswordUtil;
+import com.refactoringhabit.common.utils.TokenUtil;
 import com.refactoringhabit.member.domain.entity.Member;
 import com.refactoringhabit.member.domain.exception.NotFoundEmailException;
+import com.refactoringhabit.member.domain.exception.UserNotFoundException;
 import com.refactoringhabit.member.domain.repository.MemberRepository;
 import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +33,10 @@ public class AuthService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TokenUtil tokenUtil;
     private final EmailNewPasswordUtil emailNewPasswordUtil;
+    private final RedisRefreshTokenRepository redisRefreshTokenRepository;
+    private final CookieUtil cookieUtil;
 
     private final Random random = new Random();
 
@@ -37,8 +50,9 @@ public class AuthService {
     @Transactional
     public void resetPassword(String email) {
         String newPassword = createPassword();
-        Member member = memberRepository.findByEmail(email);
-        member.updatePassword(passwordEncoder.encode(newPassword));
+        Member member =
+            memberRepository.findByEmail(email).orElseThrow(UserNotFoundException::new);
+        member.changePassword(passwordEncoder.encode(newPassword));
 
         try {
             emailNewPasswordUtil.sendEmail(email, newPassword);
@@ -46,6 +60,50 @@ public class AuthService {
             log.error("[{}] {}", e.getClass().getSimpleName(), e.getMessage());
             throw new EmailingException();
         }
+    }
+
+    @Transactional
+    public SignInResponseDto authenticationAndCreateToken(
+        HttpServletResponse response, SignInRequestDto signInRequestDto) {
+
+        Member member = memberRepository.findByEmail(signInRequestDto.getEmail())
+                .orElseThrow(UserNotFoundException::new);
+        String altId = member.getAltId();
+
+        if (passwordEncoder.matches(signInRequestDto.getPassword(), member.getEncodedPassword())) {
+            TokenResponse createdToken = tokenUtil.createToken(altId);
+            String createdRefreshToken = createdToken.refreshToken();
+
+            redisRefreshTokenRepository.setRefreshToken(altId, createdRefreshToken);
+
+            cookieUtil.createRefreshTokenCookie(response, createdRefreshToken);
+
+            return SignInResponseDto.builder()
+                .tokenResponse(createdToken)
+                .nickName(member.getNickName())
+                .profileImage(member.getProfileImage())
+                .build();
+        }
+        throw new UserNotFoundException();
+    }
+
+    @Transactional
+    public TokenResponse reissueToken(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        String altId) {
+
+        if (redisRefreshTokenRepository.getRefreshToken(altId)
+            .equals(cookieUtil.getRefreshTokenInCookie(request))) {
+
+            TokenResponse createdToken = tokenUtil.createToken(altId);
+            String createdRefreshToken = createdToken.refreshToken();
+
+            redisRefreshTokenRepository.setRefreshToken(altId, createdRefreshToken);
+            cookieUtil.createRefreshTokenCookie(response, createdRefreshToken);
+            return createdToken;
+        }
+        throw new InvalidTokenException();
     }
 
     private String createPassword() {
