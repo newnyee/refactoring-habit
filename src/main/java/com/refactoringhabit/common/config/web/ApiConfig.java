7238ff1f29c0c754f12d8f27b29b0c3ce8cc7de8
newnyee <mebbun0615@gmail.com>
2024-06-10 19:09:22 +0900

    feat: config 추가
    - 새로운 인터셉터를 위한 config 추가 (ApiConfig, ViewConfig)

---
+++ src/main/java/com/refactoringhabit/common/config/web/ApiConfig.java
@@ -0,0 +1,33 @@
+package com.refactoringhabit.common.config.web;
+
+import com.refactoringhabit.common.interceptor.api.ApiAuthInterceptor;
+import com.refactoringhabit.common.interceptor.api.ApiAuthZInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApiConfig implements WebMvcConfigurer {
+
+    private final ApiAuthInterceptor apiAuthInterceptor;
+    private final ApiAuthZInterceptor apiAuthZInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 인증
+        registry.addInterceptor(apiAuthInterceptor)
+            .addPathPatterns("/api/v2/**")
+            .excludePathPatterns(
+                "/api/v2/auth/**",
+                "/api/v2/products/**",
+                "/api/v2/large-categories/**",
+                "/api/v2/members");
+
+        // 인가
+        registry.addInterceptor(apiAuthZInterceptor)
+            .addPathPatterns("/api/v2/hosts/**")
+            .excludePathPatterns("/api/v2/hosts");
+    }
+}
