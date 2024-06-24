090785601ec6984f1475d6a316d933bef3e4cd96
newnyee <mebbun0615@gmail.com>
2024-06-24 17:56:00 +0900

    feat: js 파일 추가
    - host-info-validation : 호스트 validation js 파일
    - host-join : 호스트 가입 관련 js 파일

---
+++ src/main/resources/static/js/host/host-info-validation.js
@@ -0,0 +1,247 @@
+// 호스트 정보 선택자 객체
+let hostInfo = {}
+let banksPattern = {}
+
+// 해당 선택자의 에러 메세지 요소 찾기
+const findErrorMassageElement = (selector) => {
+  return selector.closest('.item2').find('.error-message')
+}
+
+// 에러 메세지 보이기
+const showErrorMessage = (errorMessageElement, message) => {
+  errorMessageElement.css('display', 'block');
+  errorMessageElement.text(message);
+}
+
+// 에러 메세지 숨기기
+const hideErrorMessage = (errorMessageElement) => {
+  errorMessageElement.css('display', 'none')
+}
+
+// 필수 입력 확인
+const isNotBlank = (value, errorMessageElement) => {
+  if (value.length === 0 || value === '') {
+    showErrorMessage(errorMessageElement, '필수 입력란 입니다')
+    return false
+  }
+  hideErrorMessage(errorMessageElement)
+  return true
+}
+
+// 이미지 파일 검증
+const previewProfile = (imgs) => {
+  let reader = new FileReader();
+  let profileImageErrorMessage = findErrorMassageElement(hostInfo.$image)
+
+  if (imgs !== undefined && imgs.files.length > 0) {
+    let imgSize = imgs.files[0].size // 이미지 사이즈
+    let maxSize = 2 * 1024 *1024 // 2MB
+    let fileExtension = imgs.files[0].name.split('\.').slice(-1)[0] // 파일 확장자
+
+    if (imgSize > maxSize || (fileExtension !== 'png' && fileExtension !== 'jpg')) {
+      showErrorMessage(profileImageErrorMessage, '2MB 이하의 png, jpg 파일만 가능합니다')
+      imgs = undefined
+      return false
+    }
+
+    reader.onload = (img) => {
+      $('#preview').attr('src', img.target.result)
+    }
+    reader.readAsDataURL(imgs.files[0]);
+  } else {
+    $('#preview').attr('src', '/img/noimg.png')
+  }
+
+  hideErrorMessage(profileImageErrorMessage)
+  return true
+}
+
+// 닉네임 검증
+const nickNameValidation = () => {
+  let nickNameValue = hostInfo.$nickName.val()
+  let nickNameErrorMessage = findErrorMassageElement(hostInfo.$nickName)
+
+  if (!isNotBlank(nickNameValue, nickNameErrorMessage)) {
+    return false
+  }
+
+  if (nickNameValue.length === 1 || nickNameValue.length > 8) {
+    showErrorMessage(nickNameErrorMessage, '닉네임은 2 ~ 8자 입니다');
+    return false
+  }
+
+  let nickNameCheck = /^[ㄱ-ㅎ가-힣a-zA-Z0-9_]+$/
+  if (!nickNameCheck.test(nickNameValue) && nickNameValue.length > 0) {
+    showErrorMessage(nickNameErrorMessage,'"_" 외의 특수문자는 사용할 수 없습니다')
+    return false
+  }
+
+  hideErrorMessage(nickNameErrorMessage)
+  return true
+}
+
+// 이메일 검증
+const emailValidation = () => {
+  let emailValue = hostInfo.$email.val()
+  let emailErrorMessage = findErrorMassageElement(hostInfo.$email)
+
+  let emailCheck = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/
+  if (emailCheck.test(emailValue) === false && emailValue.length > 0) {
+    showErrorMessage(emailErrorMessage, '이메일 형식에 맞게 입력해주세요')
+    return false
+  }
+
+  hideErrorMessage(emailErrorMessage)
+  return true
+}
+
+// 공개 연락처 검증
+const phoneValidation = () => {
+  let phoneValue = hostInfo.$phone.val()
+  let phoneErrorMessage = findErrorMassageElement(hostInfo.$phone)
+
+  let containsSpecialChars = /-/
+  if (containsSpecialChars.test(phoneValue)) {
+    phoneErrorMessage.css('display', 'block')
+    phoneErrorMessage.text('"-"기호를 제외한 번호만 입력해주세요')
+    return false
+  }
+
+  let phoneCheck = /^(010)[0-9]{3,4}[0-9]{4}$/
+  if (!phoneCheck.test(phoneValue) && phoneValue.length > 0) {
+    showErrorMessage(phoneErrorMessage, '연락처를 확인해주세요')
+    return false
+  }
+
+  hideErrorMessage(phoneErrorMessage)
+  return true
+}
+
+// 소개글 검증
+const introductionValidation = () => {
+  let introduction = hostInfo.$introduction
+  let introductionErrorMessage = findErrorMassageElement(hostInfo.$introduction)
+
+  if (introduction.val().length > 500) {
+    introduction.val(introduction.val().slice(0, 500))
+  }
+  hostInfo.$introductionLength.text(introduction.val().length)
+
+  if (!isNotBlank(introduction.val(), introductionErrorMessage)) {
+    return false
+  }
+
+  if (introduction.val().length > 0 && introduction.val().length < 5) {
+    showErrorMessage(introductionErrorMessage, '소개글을 5자 이상 입력해주세요');
+    return false
+  }
+
+  hideErrorMessage(introductionErrorMessage)
+  return true
+}
+
+// 예금주 검증
+const accountHolderValidation = () => {
+  let accountHolderValue = hostInfo.$accountHolder.val()
+  let accountHolderErrorMessage = findErrorMassageElement(hostInfo.$accountHolder)
+
+  if (!isNotBlank(accountHolderValue, accountHolderErrorMessage)) {
+    return false
+  }
+
+  if (accountHolderValue.length < 2 || accountHolderValue.length > 10) {
+    showErrorMessage(accountHolderErrorMessage, '예금주는 2 ~ 10자 입니다');
+    return false
+  }
+
+  let accountHolderCheck = /^[ㄱ-ㅎ가-힣a-zA-Z!@#$%^&*()_\-\[\]=+~'";:,<>./?]+$/
+  if (!accountHolderCheck.test(accountHolderValue)) {
+    showErrorMessage(accountHolderErrorMessage,'사용할 수 없는 문자입니다')
+    return false
+  }
+
+  hideErrorMessage(accountHolderErrorMessage);
+  return true
+}
+
+// 은행 선택 검증
+const selectBankValidation = () => {
+  let bankValue = hostInfo.$bank.val()
+  let bankErrorMessage = findErrorMassageElement(hostInfo.$bank)
+
+  if (bankValue === '0') {
+    showErrorMessage(bankErrorMessage, '은행을 선택해주세요')
+    return false
+  }
+
+  hideErrorMessage(bankErrorMessage)
+  return true
+}
+
+// 계좌번호 검증 → 은행 선택이 선행되야함
+const accountNumberValidation = () => {
+
+  let accountNumberCheck = /[^0-9]/g
+  hostInfo.$accountNumber.val(hostInfo.$accountNumber.val().replace(accountNumberCheck, ''))
+
+  if (!selectBankValidation()) {
+    return false
+  }
+
+  let bankValue = hostInfo.$bank.val()
+  let accountNumberValue = hostInfo.$accountNumber.val()
+  let accountNumberErrorMessage = findErrorMassageElement(hostInfo.$accountNumber)
+
+  let accountNumberPatternCheck = banksPattern[bankValue];
+  if (!accountNumberPatternCheck.test(accountNumberValue)) {
+    showErrorMessage(accountNumberErrorMessage, '선택한 은행의 계좌번호 형식과 맞지 않습니다')
+    return false
+  }
+
+  hideErrorMessage(accountNumberErrorMessage)
+  return true
+}
+
+// 은행 목록 가져오기
+const getBankList = () => {
+  $.ajax({
+    url: '/api/v2/banks',
+    method: 'GET',
+    dataType: 'json',
+    success: (response) => {
+      banks = response.data
+      for (const bank of banks) {
+        $('#bank').append("<option value='"+ bank.code +"'>" + bank.name + "</option>")
+      }
+    }
+  })
+}
+
+$(document).ready(() => {
+  // 호스트 정보 선택자 객체
+  hostInfo = {
+    $image: $('#image'),
+    $preview: $('#preview'),
+    $nickName: $('#nick-name'),
+    $email: $('#email'),
+    $phone: $('#phone'),
+    $introduction: $('#introduction'),
+    $introductionLength: $('#introduction-length'),
+    $bank: $('#bank'),
+    $accountHolder: $('#account-holder'),
+    $accountNumber: $('#account-number')
+  }
+
+  // 은행 정규표현식 리스트
+  banksPattern = {
+    '011' : /^[0-9]{3}[0-9]{4}[0-9]{4}[0-9]{2}$/,
+    '004' : /^[0-9]{6}[0-9]{2}[0-9]{6}$/,
+    '020' : /^[0-9]{4}[0-9]{3}[0-9]{6}$/,
+    '088' : /^[0-9]{3}[0-9]{3}[0-9]{6}$/,
+    '003' : /^[0-9]{3}[0-9]{6}[0-9]{2}[0-9]{3}$/,
+    '081' : /^[0-9]{3}[0-9]{6}[0-9]{5}$/,
+    '090' : /^[0-9]{4}[0-9]{2}[0-9]{7}$/
+  }
+
+  getBankList()
+})
--- src/main/resources/static/js/host/host-join.js
+++ src/main/resources/static/js/host/host-join.js
@@ -1,44 +1,116 @@
-// 이미지 파일 체크 (onChange)
-function imgCheck(img) {
-  let host_img = document.getElementById('host_img')
-  let host_img_small = document.getElementById('host_img_small')
-  let fileInfo = img.files[0].name.split('\.')
-  let fileExtension = fileInfo[fileInfo.length-1] // 파일 확장자
+// 닉네임 중복 확인
+const nickNameDuplicateCheck = () => {
+  let nickName = hostInfo.$nickName
+  let nickNameErrorMessage = findErrorMassageElement(nickName)
+  let nickNameDuplicateCheckButton = $('#nick_name_duplicate_check_button')
+  let nickNameDuplicateSuccessMassage = $('#nick_name_duplicate_success')
 
-  if(fileExtension !== 'jpg' && fileExtension !== 'png') { // 확장자가 jpg 또는 png가 아닐경우
-    document.getElementById('preview').setAttribute('src', 'img/host-default-img.webp')
-    img.outerHTML = img.outerHTML; // 선택된 input:type=file 초기화
-    host_img_small.removeAttribute('hidden')
-    document.getElementById('host_img').focus()
-    return
-  } else {
-    host_img_small.setAttribute('hidden', true)
+  if (nickNameDuplicateCheckButton.text() === '닉네임 변경') {
+    nickNameDuplicateSuccessMassage.css('display', 'none')
+    nickNameDuplicateCheckButton.text('중복확인')
+    nickNameDuplicateCheckButton.removeClass('btn-outline-primary').addClass('btn-primary')
+    nickName.prop('disabled', false)
+    return false
   }
-  
-  let imgSize = img.files[0].size // 이미지 용량
-  let maxSize = 2 * 1024 * 1024 // 2MB
 
-  if(imgSize>maxSize) { // 이미지 용량이 2MB가 넘을 경우
-    document.getElementById('preview').setAttribute('src', 'img/host-default-img.webp')
-    img.outerHTML = img.outerHTML; // 선택된 input:type=file 초기화
-    host_img_small.removeAttribute('hidden')
-    host_img.focus()
-    return
-  } else { // 선택된 이미지 미리보기 실행
-    host_img_small.setAttribute('hidden', true)
-    if (img.files && img.files[0]) {
-      const reader = new FileReader()
-      reader.onload = function(e) {
-        document.getElementById('preview').src = e.target.result
+  if (!nickNameValidation()) {
+    return false
+  }
+
+  $.ajax({
+    url: '/api/v2/hosts/check-nick-name',
+    method: 'GET',
+    data: {
+      nickName: nickName.val()
+    },
+    success: (response) => {
+      if (response.data) {
+        nickNameErrorMessage.css('display', 'block')
+        nickNameErrorMessage.text('❌이미 사용중인 이메일입니다')
+      } else {
+        nickNameErrorMessage.css('display', 'none')
+        nickName.prop('disabled', true)
+
+        nickNameDuplicateCheckButton.text('닉네임 변경')
+        nickNameDuplicateCheckButton.removeClass('btn-primary').addClass('btn-outline-primary')
+        nickNameDuplicateSuccessMassage.css('display', 'block')
       }
-      reader.readAsDataURL(img.files[0])
-    } else {
-      document.getElementById('preview').src = ""
+    },
+    error: (e) => {
+      if (e.responseJSON.status === 500) {
+        alert("오류가 발생했습니다. 관리자에게 문의하세요.")
+      }
     }
+  })
+}
+
+// 호스트 가입 api 호출
+const callHostJoinApi = () => {
+
+  let requestHostInfo = {
+    nickName: hostInfo.$nickName.val(),
+    email: hostInfo.$email.val(),
+    phone: hostInfo.$phone.val(),
+    introduction: hostInfo.$introduction.val(),
+    bank: hostInfo.$bank.val(),
+    accountHolder: hostInfo.$accountHolder.val(),
+    accountNumber: hostInfo.$accountNumber.val()
   }
+
+  let formData = new FormData()
+  let requestHostInfoJson = JSON.stringify(requestHostInfo)
+  let blob = new Blob([requestHostInfoJson], {type: "application/json"})
+
+  let profileImgFiles = hostInfo.$image[0].files;
+  let profileImgFile
+      = (profileImgFiles.length !== 0)
+      ? profileImgFiles[0]
+      : null
+
+  formData.append('hostInfo', blob)
+  formData.append('profileImgFile', profileImgFile)
+
+  $.ajax({
+    url: '/api/v2/hosts',
+    method: 'POST',
+    enctype: 'multipart/form-data',
+    processData: false,
+    contentType: false,
+    data: formData,
+    success: () => {
+      alert("호스트 가입이 완료되었습니다.")
+      window.location.replace("/host")
+    },
+    error: (e) => {
+      if (e.responseJSON.status === 500) {
+        alert("오류가 발생했습니다. 관리자에게 문의하세요.")
+      }
+    }
+  })
 }
 
+$(document).ready(() => {
 
-const profileCheck = () => {
- location.href = "host-home.html"
-}
+  // 호스트 가입 버튼 클릭
+  $('#host-join-submit-button').on('click', () => {
+
+    // 닉네임 중복 검사 확인
+    let nickName = hostInfo.$nickName
+    if (!nickName.prop('disabled')) {
+      let nickNameErrorMessage = findErrorMassageElement(nickName)
+      showErrorMessage(nickNameErrorMessage)
+      return false
+    }
+
+    // 유효성 검사
+    if (!(nickNameValidation() && emailValidation()
+        && phoneValidation() && introductionValidation()
+        && accountHolderValidation() && selectBankValidation()
+        && accountNumberValidation())) {
+      return false
+    }
+
+    // 호스트 가입 api 호출
+    callHostJoinApi()
+  })
+})
