b2ef75c354dbe6683b57da5d0bbbdec6ee39e430
newnyee <mebbun0615@gmail.com>
2024-06-24 21:50:48 +0900

    refactor: @DynamicUpdate 어노테이션 추가
    - 수정된 필드만 포함하는 쿼리를 위해 해당 어노테이션 추가

--- src/main/java/com/refactoringhabit/host/domain/entity/Host.java
+++ src/main/java/com/refactoringhabit/host/domain/entity/Host.java
@@ -14,10 +14,12 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
 
 @Table(name = "hosts")
 @Entity
 @Getter
+@DynamicUpdate
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Host extends BaseTimeEntity {
 
