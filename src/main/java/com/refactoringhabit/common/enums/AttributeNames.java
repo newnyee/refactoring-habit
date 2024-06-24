7842ec9f9e05088c6815d105563183cf91167c3b
newnyee <mebbun0615@gmail.com>
2024-06-25 01:17:42 +0900

    test: HostService, ViewAuthZInterceptor test 코드 작성

--- src/main/java/com/refactoringhabit/common/enums/AttributeNames.java
+++ src/main/java/com/refactoringhabit/common/enums/AttributeNames.java
@@ -3,6 +3,7 @@ package com.refactoringhabit.common.enums;
 public enum AttributeNames {
 
     MEMBER_ALT_ID("memberAltId"),
+    HOST_ALT_ID("hostAltId"),
     REDIRECT_URL("redirectURL"),
     MEMBER_INFO("memberInfo"),
     HOST_INFO("hostInfo"),
