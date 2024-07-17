2cf0f3ee851d58af9a3e7145afa1ae405889e7bb
newnyee <mebbun0615@gmail.com>
2024-07-17 19:47:06 +0900

    refactor: step 추가

--- src/main/java/com/refactoringhabit/common/config/batch/job/DailyBatchJobConfig.java
+++ src/main/java/com/refactoringhabit/common/config/batch/job/DailyBatchJobConfig.java
@@ -1,26 +1,27 @@
 package com.refactoringhabit.common.config.batch.job;
 
 import com.refactoringhabit.common.config.batch.listener.CustomJobListener;
-import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class DailyBatchJobConfig {
 
     @Bean
     public Job dailyStatsJob(CustomJobListener jobListener, JobRepository jobRepository,
-        Step productTotalSalesStatsUpdateStep) {
+        @Qualifier("productTotalSalesStatsUpdateStep") Step productTotalSalesStatsUpdateStep,
+        @Qualifier("hostTotalSalesStatsUpdateStep") Step hostTotalSalesStatsUpdateStep) {
         return new JobBuilder("statsJob", jobRepository)
             .incrementer(new RunIdIncrementer())
             .listener(jobListener)
             .start(productTotalSalesStatsUpdateStep)
+            .next(hostTotalSalesStatsUpdateStep)
             .build();
     }
 }
