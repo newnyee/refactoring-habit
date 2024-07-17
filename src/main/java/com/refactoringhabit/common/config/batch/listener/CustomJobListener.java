b892796caac5c76a77c7114f03629d44b745ee2a
newnyee <mebbun0615@gmail.com>
2024-07-17 19:37:17 +0900

    refactor: 코드 삭제
    - 배치 기본 로깅에 실행시간이 포함되어 해당 로직 삭제

--- src/main/java/com/refactoringhabit/common/config/batch/listener/CustomJobListener.java
+++ src/main/java/com/refactoringhabit/common/config/batch/listener/CustomJobListener.java
@@ -1,8 +1,5 @@
 package com.refactoringhabit.common.config.batch.listener;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
@@ -24,18 +21,6 @@ public class CustomJobListener implements JobExecutionListener {
     public void afterJob(JobExecution jobExecution) {
         if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
             log.info("{} completed", jobExecution.getJobInstance().getJobName());
-
-            LocalDateTime startTime = jobExecution.getStartTime();
-            LocalDateTime endTime = LocalDateTime.now();
-
-            long totalExecutionMilliSeconds = Duration.between(startTime, endTime).toMillis();
-
-            log.info("{} total execution time : {}H {}m {}s {}ms",
-                jobExecution.getJobInstance().getJobName(),
-                TimeUnit.MILLISECONDS.toHours(totalExecutionMilliSeconds),
-                TimeUnit.MILLISECONDS.toMinutes(totalExecutionMilliSeconds) % 60,
-                TimeUnit.MILLISECONDS.toSeconds(totalExecutionMilliSeconds) % 60,
-                totalExecutionMilliSeconds % 1000);
         }
     }
 }
--- src/main/java/com/refactoringhabit/common/config/batch/listener/CustomStepListener.java
+++ src/main/java/com/refactoringhabit/common/config/batch/listener/CustomStepListener.java
@@ -1,8 +1,5 @@
 package com.refactoringhabit.common.config.batch.listener;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
@@ -25,18 +22,6 @@ public class CustomStepListener implements StepExecutionListener {
     public ExitStatus afterStep(StepExecution stepExecution) {
         if (stepExecution.getStatus() == BatchStatus.COMPLETED) {
             log.info("{} completed", stepExecution.getStepName());
-
-            LocalDateTime startTime = stepExecution.getStartTime();
-            LocalDateTime endTime = LocalDateTime.now();
-
-            long totalExecutionMilliSeconds = Duration.between(startTime, endTime).toMillis();
-
-            log.info("{} total execution time : {}H {}m {}s {}ms",
-                stepExecution.getStepName(),
-                TimeUnit.MILLISECONDS.toHours(totalExecutionMilliSeconds),
-                TimeUnit.MILLISECONDS.toMinutes(totalExecutionMilliSeconds) % 60,
-                TimeUnit.MILLISECONDS.toSeconds(totalExecutionMilliSeconds) % 60,
-                totalExecutionMilliSeconds % 1000);
         }
         return ExitStatus.COMPLETED;
     }
