9f8b84e6d4bfadf696136af3f53d69c9029716d2
newnyee <mebbun0615@gmail.com>
2024-07-26 06:18:30 +0900

    test: test 추가
    - 인기 상품, 신규 상품 캐시 갱신 스케줄러 단위 테스트 추가(mock)

---
+++ src/test/java/com/refactoringhabit/product/domain/service/ProductServiceMockTest.java
@@ -0,0 +1,53 @@
+package com.refactoringhabit.product.domain.service;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.refactoringhabit.common.domain.repository.RedisRepository;
+import com.refactoringhabit.product.domain.repository.ProductRepository;
+import com.refactoringhabit.product.dto.HomeProductDto;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceMockTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @Mock
+    private RedisRepository redisRepository;
+
+    @Mock
+    private List<HomeProductDto> productDtos;
+
+    @InjectMocks
+    private ProductService productService;
+
+    private static final String CACHE_NAME_POPULAR_PRODUCT = "popular-products";
+    private static final String CACHE_NAME_NEW_PRODUCT = "new-products";
+    private static final String CACHE_KEY_NAME = "SimpleKey []";
+
+    @Test
+    void testUpdatePopularProductsCache() {
+        when(productRepository.productsOrderBySalesVolumeAndReviewAverage())
+            .thenReturn(productDtos);
+
+        productService.updatePopularProductsCache();
+        verify(redisRepository)
+            .setCache(CACHE_NAME_POPULAR_PRODUCT, CACHE_KEY_NAME, productDtos);
+    }
+
+    @Test
+    void testUpdateNewProductsCache() {
+        when(productRepository.productsOrderByCreatedAt()).thenReturn(productDtos);
+
+        productService.updateNewProductsCache();
+        verify(redisRepository)
+            .setCache(CACHE_NAME_NEW_PRODUCT, CACHE_KEY_NAME, productDtos);
+    }
+}
