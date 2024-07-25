52ec3887ce27c622b99465bdca5de89c8d4d885f
newnyee <mebbun0615@gmail.com>
2024-07-25 21:12:10 +0900

    feat: 홈 화면 인기상품, 신규상품 api 호출 로직 추가

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,3 +1,117 @@
+const getImageFileNameList = (imageFileNames) => {
+    return imageFileNames.split("|")
+}
+
+const formatCurrency = (amount) => {
+    return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + ' 원';
+}
+
+const createStarScoreImage = (starScore) => {
+    let starScoreImage = '';
+    if (starScore > 0) {
+        let fullStar = '                                <img src="../../img/star.png" alt="" class="Home_recommend_star">\n';
+        let halfStar = '                                <img src="../../img/halfstar.png" alt="" class="Home_recommend_star">\n'
+        let fullStarCount = Math.floor(starScore)
+        let hasHalfStar = (starScore - fullStarCount) >= 0.5
+
+        for (let i = 0; i < fullStarCount; i++) {
+            starScoreImage += fullStar
+        }
+
+        if (hasHalfStar) {
+            starScoreImage += halfStar
+        }
+    }
+
+    return starScoreImage
+}
+
+const createProductElement = (product) => {
+    const imageFileNameList = getImageFileNameList(product.imageFileNames)
+    return '    <div class="Home_product_recommend_p">\n'
+        + '        <a href="/products/' + product.productAltId + '" class="href">\n'
+        + '            <div class="Home_product_recommend_p_div">\n'
+        + '                <div>\n'
+        + '                    <img src="/storage/'+ imageFileNameList[0] +'" width="150px" height="150px">\n'
+        + '                </div>\n'
+        + '                <div>\n'
+        + '                    <button class="Home_product_recommend_p_div_btn zzim_btn" onclick="hello()" onsubmit="return false">\n'
+        + '                        <img src="../../img/black2.png" alt="" width="40px" class="Home_product_recommend_p_div_img">\n'
+        + '                    </button>\n'
+        + '                </div>\n'
+        + '                <div class="Home_product_recommend_p_font">\n'
+        + '                    <div>\n'
+        + '                        <div>' + product.name + '</div>\n'
+        + '                        <section class="Home_recommend_img">\n'
+        + '                            <div>\n'
+        + createStarScoreImage(product.reviewAverage)
+        + '                            </div>\n'
+        + '                            <div class="review-count">\n'
+        + '                                <span>리뷰 ' + product.reviewCount + '</span>\n'
+        + '                            </div>\n'
+        + '                        </section>\n'
+        + '                        <hr class="Home_recommend_hr">\n'
+        + '                        <div>' + formatCurrency(product.minPrice) + '</div>\n'
+        + '                    </div>\n'
+        + '                </div>\n'
+        + '            </div>\n'
+        + '        </a>\n'
+        + '    </div>'
+}
+
+const addProducts = (containerElement, productList) => {
+    for (let i = 0; i < 4; i++) {
+        containerElement.append(createProductElement(productList[i]))
+    }
+}
+
+const callPopularProductsApi = () => {
+    let popularProductContainer = $('.popular-product')
+    $.ajax({
+        url: '/api/v2/products/popular',
+        method: 'GET',
+        success: (response) => {
+            console.log(response.data)
+            addProducts(popularProductContainer, response.data)
+        },
+        error: (e) => {
+            if (e.responseJSON.status === 500) {
+                alert("오류가 발생했습니다. 관리자에게 문의하세요.")
+            }
+        }
+    })
+}
+
+const callNewProductsApi = () => {
+    let newProductContainer = $('.new-product')
+    $.ajax({
+        url: '/api/v2/products/new',
+        method: 'GET',
+        success: (response) => {
+            console.log(response.data)
+            addProducts(newProductContainer, response.data)
+        },
+        error: (e) => {
+            if (e.responseJSON.status === 500) {
+                alert("오류가 발생했습니다. 관리자에게 문의하세요.")
+            }
+        }
+    })
+}
+
+$(document).ready(() => {
+    callPopularProductsApi()
+    callNewProductsApi()
+
+    $('.see-more-popular-products').on('click', () => {
+        window.location.href = '/products/popular-list'
+    })
+
+    $('.see-more-new-products').on('click', () => {
+        window.location.href = '/products/new-list'
+    })
+})
+
 $(document).ready(function(){
 
     var btn=$('.zzim_btn');
@@ -159,3 +273,4 @@ function closeEvent(){
     }
     $('.intro_modal').css('display','none');
 }
+
