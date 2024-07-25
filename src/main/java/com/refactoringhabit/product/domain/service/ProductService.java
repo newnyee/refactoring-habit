99e7b917f3b4cde3e8ed89efd458f5c6ef29305a
newnyee <mebbun0615@gmail.com>
2024-07-26 06:17:38 +0900

    feat: 인기 상품, 신규 상품 캐시 갱신 로직 추가
    - 스케줄러 이용하여 인기 상품은 1일, 신규 상품은 1시간 마다 갱신

--- src/main/java/com/refactoringhabit/product/domain/service/ProductService.java
+++ src/main/java/com/refactoringhabit/product/domain/service/ProductService.java
@@ -1,28 +1,53 @@
 package com.refactoringhabit.product.domain.service;
 
+import com.refactoringhabit.common.domain.repository.RedisRepository;
 import com.refactoringhabit.product.dto.HomeProductDto;
 import com.refactoringhabit.product.domain.repository.ProductRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final RedisRepository redisRepository;
+
+    private static final String CACHE_NAME_POPULAR_PRODUCT = "popular-products";
+    private static final String CACHE_NAME_NEW_PRODUCT = "new-products";
+    private static final String CACHE_KEY_NAME = "SimpleKey []";
 
     @Transactional(readOnly = true)
-    @Cacheable(value = "popular-products", cacheManager = "contentCacheManager")
+    @Cacheable(value = CACHE_NAME_POPULAR_PRODUCT, cacheManager = "contentCacheManager")
     public List<HomeProductDto> getPopularProducts() {
         return productRepository.productsOrderBySalesVolumeAndReviewAverage();
     }
 
     @Transactional(readOnly = true)
-    @Cacheable(value = "new-products", cacheManager = "contentCacheManager")
+    @Cacheable(value = CACHE_NAME_NEW_PRODUCT, cacheManager = "contentCacheManager")
     public List<HomeProductDto> getNewProducts() {
         return productRepository.productsOrderByCreatedAt();
+    }
+
+    @Scheduled(cron = "0 0 1 * * ?")
+    public void updatePopularProductsCache() {
+        log.info("Updating popular products cache ...");
+        List<HomeProductDto> popularProducts =
+            productRepository.productsOrderBySalesVolumeAndReviewAverage();
+        redisRepository.setCache(CACHE_NAME_POPULAR_PRODUCT, CACHE_KEY_NAME, popularProducts);
+    }
+
+    @Scheduled(cron = "0 0 * * * ?")
+    public void updateNewProductsCache() {
+        log.info("Updating new products cache ...");
+        List<HomeProductDto> newProducts =
+            productRepository.productsOrderByCreatedAt();
+        redisRepository.setCache(CACHE_NAME_NEW_PRODUCT, CACHE_KEY_NAME, newProducts);
     }
 }
