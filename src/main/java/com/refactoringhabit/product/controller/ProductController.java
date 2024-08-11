8abe5f026d43e0db1644e0f9c145f4a1386a096c
newnyee <mebbun0615@gmail.com>
2024-08-12 03:13:58 +0900

    feat: 상품 상세 페이지 핸들러 메서드 추가

--- src/main/java/com/refactoringhabit/product/controller/ProductController.java
+++ src/main/java/com/refactoringhabit/product/controller/ProductController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,5 +27,16 @@ public class ProductController {
         model.addAttribute("pageTitle", "신규 해빗");
         model.addAttribute("productList", productService.getNewProducts());
         return "/pages/home-product-list";
+    }
+
+    @GetMapping("/{productId}")
+    public String productDetail(@PathVariable("productId") String productAltId, Model model) {
+        model.addAttribute("productId", productAltId);
+        return "/pages/product/product-detail";
+    }
+
+    @GetMapping("/{productId}/review")
+    public String review(@PathVariable("productId") String productAltId, Model model) {
+        return "/pages/product/product-review-list";
     }
 }
