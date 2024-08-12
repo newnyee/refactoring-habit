ed4109aeba285fbc3c8e6f597e382a6be8a4137a
newnyee <mebbun0615@gmail.com>
2024-08-13 07:23:26 +0900

    test: test 코드 추가

--- src/test/java/com/refactoringhabit/review/domain/service/ReviewServiceTest.java
+++ src/test/java/com/refactoringhabit/review/domain/service/ReviewServiceTest.java
@@ -26,15 +26,42 @@ class ReviewServiceTest {
     private ReviewService reviewService;
 
     @Test
-    @DisplayName("상품 아이디에 따른 리뷰 목록 얻기")
-    void testGetReviewDetailDtoListByProductId() {
+    @DisplayName("상품 아이디에 따른 리뷰 목록 얻기 - two parameters")
+    void testGetReviewDetailDtoListByProductId_TwoParameters() {
         List<ReviewDetailDto> reviewDetailDtoList = mock(List.class);
         Pageable pageable = mock(Pageable.class);
-        when(reviewRepository.findByProductIdLimit(PRODUCT_ALT_ID.getName(), pageable))
+        String orderByValue = "createAt";
+        when(reviewRepository
+            .findByProductIdLimit(PRODUCT_ALT_ID.getName(), pageable, orderByValue))
             .thenReturn(reviewDetailDtoList);
 
         List<ReviewDetailDto> getReviewDetailDtoList = reviewService.getReviewDetailDtoList(
             PRODUCT_ALT_ID.getName(), pageable);
         assertEquals(reviewDetailDtoList, getReviewDetailDtoList);
+    }
+
+    @Test
+    @DisplayName("상품 아이디에 따른 리뷰 목록 얻기 - three parameters")
+    void testGetReviewDetailDtoListByProductId_ThreeParameters() {
+        List<ReviewDetailDto> reviewDetailDtoList = mock(List.class);
+        Pageable pageable = mock(Pageable.class);
+        String orderByValue = "createAt";
+        when(reviewRepository
+            .findByProductIdLimit(PRODUCT_ALT_ID.getName(), pageable, orderByValue))
+            .thenReturn(reviewDetailDtoList);
+
+        List<ReviewDetailDto> getReviewDetailDtoList = reviewService.getReviewDetailDtoList(
+            PRODUCT_ALT_ID.getName(), pageable, orderByValue);
+        assertEquals(reviewDetailDtoList, getReviewDetailDtoList);
+    }
+
+    @Test
+    @DisplayName("상품 아이디에 따른 리뷰 수 얻기")
+    void testGetReviewCount() {
+        Long reviewCount = 10L;
+        when(reviewRepository.countByProductId(PRODUCT_ALT_ID.getName())).thenReturn(reviewCount);
+
+        Long getReviewCount = reviewService.getReviewCount(PRODUCT_ALT_ID.getName());
+        assertEquals(reviewCount, getReviewCount);
     }
 }
